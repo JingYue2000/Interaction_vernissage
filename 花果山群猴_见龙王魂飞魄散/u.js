cccc46a1ddfac6aa3b250da0b184ef63ac56b014
Munyeong Kim <58014011+MunyeongKim@users.noreply.github.com>
2026-03-31 15:45:08 -0400

    Animate mountain over cached lightning layer

--- 花果山群猴_见龙王魂飞魄散/u.js
+++ 花果山群猴_见龙王魂飞魄散/u.js
@@ -1,13 +1,20 @@
-const c=document.getElementById("c"),x=c.getContext("2d"),C=Math.cos,T=Math.tan,R=(r=0,g=0,b=0,a=1)=>`rgba(${Math.max(0,r)},${Math.max(0,g)},${Math.max(0,b)},${a})`
-let f,i,w,火
-function u(t) {
-  x.save()
-  with(x)for(i=800;i--;fillRect(~setTransform(i,0,0,i,880+i+3*i*C(i),700+2*i*C(i/400)),~rotate(i/6+t/2),2,2))fillStyle=R(i/5,i/4,i/3,.07)
-  x.restore()
+const c=document.getElementById("c"),x=c.getContext("2d"),l=Object.assign(document.createElement("canvas"),{width:c.width,height:c.height}),y=l.getContext("2d"),C=Math.cos,T=Math.tan,R=(r=0,g=0,b=0,a=1)=>`rgba(${Math.max(0,r)},${Math.max(0,g)},${Math.max(0,b)},${a})`
+let f,i,j,w,火,a
+function v() {
   f=(X,Y,n=0)=>X*X>4?n:f(2*X*Y-.7,T(Y*Y-X*X),n+1)
-  for(i=w=2e3;i--;x.fillRect(i,t*60,1,火))
-  x.fillStyle=R(火=f(i/w,t/28-.1)**1.6,火/6,0,.18)
+  for(j=0;j<l.height;j+=2)for(i=0,w=l.width;i<w;i+=2)火=f(i/w,j/1680-.1)**1.6,火>11e2&&(y.fillStyle=R(160+火/30,200+火/18,255,.08),y.fillRect(i,j,2,2+火/900))
 }
-x.fillStyle="#000"
-x.fillRect(0,0,c.width,c.height)
+function u(t) {
+  x.fillStyle="#000"
+  x.fillRect(0,0,c.width,c.height)
+  x.save()
+  with(x)for(i=800;i--;fillRect(~setTransform(i,0,0,i,880+i+3*i*C(i),700+2*i*C(i/400)),~rotate(i/6+t/2),2,2))fillStyle=R(i/5,i/4,i/3,.05)
+  x.restore()
+  x.save()
+  x.globalCompositeOperation="screen"
+  x.globalAlpha=a=.02+.78*Math.max(0,Math.sin(t*3))**10
+  x.drawImage(l,0,0)
+  x.restore()
+}
+v()
 requestAnimationFrame(function f(ms){u(ms/1e3),requestAnimationFrame(f)})
